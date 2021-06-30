f36d1d962d96f9afaa77c540b6cd82a47fe1bd0e
Alexis-Diaz <alex.d4556@gmail.com>
2021-06-29 21:10:01 -0600

    Se agregó un archivo al gitignore

---
+++ Portafolio/Env.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Portafolio
+{
+   public class Env
+   {
+        public string transport = "softwaredevelopment992@gmail.com";
+        public string destinatario = "alex.d4556@gmail.com";
+        public string password = "adminnegocio2021";
+ 
+   }
+}
